f4c7335ccb1e101fc400439bae0ed36240b2f698
System Administrator <root@Giovannys-MacBook.local>
2021-03-09 09:54:54 -0500

    VigHub version 2.0(minimize expression data)

--- Connections/crenesoftservidor.php
+++ Connections/crenesoftservidor.php
@@ -5,10 +5,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_crenesoft = "mysql10.000webhost.com";
-$database_crenesoft = "a3020425_crene";
-$username_crenesoft = "a3020425_crene";
-$password_crenesoft = "holamundo123";
+$hostname_crenesoft = "nameserver";
+$database_crenesoft = "database";
+$username_crenesoft = "user";
+$password_crenesoft = "pass";
 $crenesoft = mysql_pconnect($hostname_crenesoft, $username_crenesoft, $password_crenesoft) or trigger_error(mysql_error(),E_USER_ERROR);
 ?>
 
